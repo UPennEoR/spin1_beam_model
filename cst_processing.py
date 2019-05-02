2987cbe4e712e817e39e45462094de47ca5852a4
Zachary Martinot <zmartinot@gmail.com>
2019-05-02 15:20:46 -0600

    completed CSTDataProcessor and a basic class (AntennaFarFieldResponse) to read in the model data and compute spline approximations. Made a start on some basic convinience evaluation methods, will need more work.

--- cst_processing.py
+++ cst_processing.py
@@ -7,34 +7,8 @@ import pyssht
 
 import ssht_numba as sshtn
 
-@nb.njit
-def Elm_fourier_series(nu_axis, Elm_ft, nu_0, bandwidth):
-    """
-    Evaluates the fourier series which defines the frequency spectrum
-    of each spatial harmonic mode of the model.
-    """
-    Nfreq = nu_axis.size
-
-    Nft, Nmodes = Elm_ft.shape
-    T = 2*bandwidth
-    Elm = np.zeros((Nfreq, Nmodes), dtype=nb.complex128)
-
-    k = np.arange(Nft)
-
-    for ii in range(Nfreq):
-        nu_i = (nu_axis[ii] - nu_0)
-        arg = 2*np.pi * k * nu_i/T
-        kernel = np.cos(arg) + 1j*np.sin(arg)
-        for jj in range(Nmodes):
-            for kk in range(Nft):
-                Elm[ii,jj] += Elm_ft[kk,jj] * kernel[kk]
-
-    Elm /= Nft
-
-    return Elm
-
-def periodic_extension(Elm_in):
-    Nfreq, Nmode = Elm_in.shape[0]
+def reflected_extension(Elm_in):
+    Nfreq, Nmode = Elm_in.shape
 
     Elm_r = np.zeros((2*Nfreq-2, Nmode), dtype=np.complex128)
 
@@ -59,6 +33,9 @@ class CSTDataProcessor(object):
     coefficients that define a global beam model on the frequency domain
     supported by the input data.
 
+    The fields are not normalized here, they are to be normalized once a global
+    model is fit to these data.
+
     Inputs:
     data_files: a list of strings, sorted by frequency, where each element is
         the full path to the CST text file with the E-Field data at that
@@ -66,10 +43,14 @@ class CSTDataProcessor(object):
     data_freqs: a numpy array of floats, with elements corresponding to the
         frequency in MHz of the file in the data_files list.
     spatial_sampling: the equiangular resolution of the data files, in degrees.
+    data_uncertainty: the fractional accuracy of the input CST data.
+        Example:
+            If the CST solver was run with an accuracy setting of 40dB, then
+            data_uncertainty should be 1e-4.
 
     Assumes that all input files have the same spatial sampling resolution.
     """
-    def __init__(self, data_files, data_freqs, spatial_sampling=1.,
+    def __init__(self, data_files, data_freqs, spatial_sampling, data_uncertainty,
                 zetnith_theta=0., zenith_phi=0.,
                 data_source_notes_string='left blank by user'):
 
@@ -84,6 +65,8 @@ class CSTDataProcessor(object):
         self.data_files = data_files
         self.data_freqs = data_freqs
 
+        self.data_uncertainty = data_uncertainty
+
         self.Nfreq = data_freqs.size
 
         data_file = np.loadtxt(data_files[0], skiprows=2)
@@ -94,19 +77,19 @@ class CSTDataProcessor(object):
         self.theta_data = np.reshape(np.radians(data_file[:,0]), (self.Nphi, self.Ntheta)).T
         self.phi_data = np.reshape(np.radians(data_file[:,1]), (self.Nphi, self.Ntheta)).T
 
-        self.L = self.Ntheta - 1 # spatial harmonic bandlimit of the data
+        self.L_data = self.Ntheta - 1 # spatial harmonic bandlimit of the data
 
         self.zenith_theta = zetnith_theta
         self.zenith_phi = zenith_phi
 
         self.overwrite = False
 
-    def compute_spin1_harmonics(self):
+    def compute_spin1_harmonics(self, filter_cut_factor=1.):
 
-        self.pos1_Elm_nodes = np.zeros((self.Nfreq, self.L**2), dtype=np.complex128)
-        self.neg1_Elm_nodes = np.zeros((self.Nfreq, self.L**2), dtype=np.complex128)
+        self.pos1_Elm_nodes = np.zeros((self.Nfreq, self.L_data**2), dtype=np.complex128)
+        self.neg1_Elm_nodes = np.zeros((self.Nfreq, self.L_data**2), dtype=np.complex128)
         self.zenith_Eabs = np.zeros(self.Nfreq, dtype=np.float64)
-        delta = pyssht.generate_dl(np.pi/2., self.L)
+        delta = pyssht.generate_dl(np.pi/2., self.L_data)
 
         for ii in range(self.Nfreq):
             data = np.loadtxt(self.data_files[ii], skiprows=2)
@@ -123,25 +106,62 @@ class CSTDataProcessor(object):
             pos1_E = (Et + 1j*Ep)/np.sqrt(2.)
             neg1_E = (Et - 1j*Ep)/np.sqrt(2.)
 
-            pos1_Elm = pyssht.forward(pos1_E, self.L, Spin=1, Method='MWSS', Reality=False)
-            neg1_Elm = pyssht.forward(neg1_E, self.L, Spin=-1, Method='MWSS', Reality=False)
-
-            # normalize to make the directivity 1 in the zenith direction
-            # note, this is not necessarily the exact peak of the function. But it
-            # should be very close, to within the precision of the CST data
-            zen_pos1_E = sshtn.ssht_numba_series_eval(pos1_Elm, 1, self.L, delta, np.array([self.zenith_theta]), np.array([self.zenith_phi]))
-            zen_neg1_E = sshtn.ssht_numba_series_eval(neg1_Elm, -1, self.L, delta, np.array([self.zenith_theta]), np.array([self.zenith_phi]))
-
-            zen_Et = (zen_pos1_E + zen_neg1_E)/np.sqrt(2.)
-            zen_Ep = (zen_pos1_E - zen_neg1_E)*(-1j)/np.sqrt(2.)
-            zen_Eabs = np.sqrt(np.abs(zen_Et)**2. + np.abs(zen_Ep)**2.)
-
-            pos1_Elm /= zen_Eabs
-            neg1_Elm /= zen_Eabs
+            pos1_Elm = pyssht.forward(pos1_E, self.L_data, Spin=1, Method='MWSS', Reality=False)
+            neg1_Elm = pyssht.forward(neg1_E, self.L_data, Spin=-1, Method='MWSS', Reality=False)
 
             self.pos1_Elm_nodes[ii] = pos1_Elm
             self.neg1_Elm_nodes[ii] = neg1_Elm
-            self.zenith_Eabs[ii] = zen_Eabs
+
+        pos1_Elm_ex = reflected_extension(self.pos1_Elm_nodes)
+        neg1_Elm_ex = reflected_extension(self.neg1_Elm_nodes)
+
+        pos1_Elm_ft = np.fft.fft(pos1_Elm_ex, axis=0)
+        neg1_Elm_ft = np.fft.fft(neg1_Elm_ex, axis=0)
+
+        Nft_mode = 2*self.Nfreq-2
+
+        pCl_tau = np.zeros((Nft_mode, self.L_data))
+        for ii in range(Nft_mode):
+            pCl_tau[ii] = ssht_power_spectrum(pos1_Elm_ft[ii])
+
+        nCl_tau = np.zeros((Nft_mode, self.L_data))
+        for ii in range(Nft_mode):
+            nCl_tau[ii] = ssht_power_spectrum(neg1_Elm_ft[ii])
+
+        Cl_tau = pCl_tau + nCl_tau
+        max_val = np.amax(Cl_tau)
+
+        eps = self.data_uncertainty
+
+        mode_filter = Cl_tau/(Cl_tau + (eps**2. * max_val))
+        cut_inds = np.where(mode_filter < (filter_cut_factor * eps))
+        mode_filter[cut_inds] = 0.
+
+        L_model = self.L_data
+        for ii in range(self.L_data):
+            # for each possible bandlimit (decending from L_data),
+            # check if all modes are zero
+            if np.count_nonzero(mode_filter[:,-(ii+1)]) == 0:
+                L_model = self.L_data - ii
+            else:
+                # the bandlimit of the filtered modes has been found, so
+                break
+
+        pos1_Elm_ft_s = np.copy(pos1_Elm_ft)
+        neg1_Elm_ft_s = np.copy(neg1_Elm_ft)
+
+        for ii in range(Nft_mode):
+            for ell in range(self.L_data):
+                m = np.arange(-ell, ell+1)
+                indices = sshtn.elm2ind(ell, m)
+                pos1_Elm_ft_s[ii, indices] *= mode_filter[ii,ell]
+                neg1_Elm_ft_s[ii, indices] *= mode_filter[ii,ell]
+
+        self.pos1_Elm_s = np.fft.ifft(pos1_Elm_ft_s[:,:L_model**2], axis=0)[:self.Nfreq]
+        self.neg1_Elm_s = np.fft.ifft(neg1_Elm_ft_s[:,:L_model**2], axis=0)[:self.Nfreq]
+
+        self.L_model = L_model
+        self.filter_cut_factor = filter_cut_factor
 
     def write_model_data(self, save_dir, save_name):
 
@@ -151,13 +171,16 @@ class CSTDataProcessor(object):
             raise ValueError('A save file with that name already exists.')
 
         with h5py.File(full_file_name, 'w') as h5f:
-            h5f.create_dataset('pos1_Elm_nodes', data=self.pos1_Elm_nodes)
-            h5f.create_dataset('neg1_Elm_nodes', data=self.neg1_Elm_nodes)
-            h5f.create_dataset('zenith_Eabs', data=self.zenith_Eabs)
-            h5f.create_dataset('frequencies', data=self.data_freqs)
+
             h5f.create_dataset('zenith_theta', data=self.zenith_theta)
             h5f.create_dataset('zenith_phi', data=self.zenith_phi)
-            h5f.create_dataset('spatial_bandlimit', data=self.L)
+            h5f.create_dataset('spatial_bandlimit', data=self.L_model)
+            h5f.create_dataset('filter_cut_factor', data=self.filter_cut_factor)
+            h5f.create_dataset('data_uncertainty', data=self.data_uncertainty)
 
             data_source_notes_string = np.string_(self.data_source_notes_string)
             h5f.create_dataset('data_source_notes', data=data_source_notes_string)
+
+            h5f.create_dataset('pos1_Elm', data=self.pos1_Elm_s)
+            h5f.create_dataset('neg1_Elm', data=self.neg1_Elm_s)
+            h5f.create_dataset('frequencies', data=self.data_freqs)
